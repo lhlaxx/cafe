77a809a8adeeb90d03f14f1842b8f1c4f0079200
unknown <lihuili@droi.com>
2018-08-25 19:28:05 +0800

    购物车

--- src/pages/menu/index.vue
+++ src/pages/menu/index.vue
@@ -13,13 +13,18 @@
         </ul>
       </aside>
       <section>
+
         <ul>
+          <div class="addgo" @click="go"><img class="gouwuche" src="../../../static/img/gouwuche.png" alt=""><span v-if="num!=0">{{num}}</span></div>
           <li v-for="(item,index) in list" :key="index">
             <img :src="item.src" alt="">
             <div class="flex">
               <p>{{item.name}}</p>
               <p><span>￥</span>{{item.price}} <span>起</span>  </p>
-              <p><span>+</span></p>
+              <p>
+                <span @click="decrease(index)" :key="index" v-if="item.num>0">-</span>
+                <i v-if="item.num !=0" :key="index" >{{item.num}}</i>
+                <span @click="increase(index)" :key="index">+</span></p>
             </div>
           </li>
         </ul>
@@ -28,24 +33,32 @@
 </template>
 
 <script>
+  import store from "../../utils/store"
   export default {
-    name: "index",
     data(){
       return{
-        list:[
-          {src:"../../../static/img/coffee1.png",name:"美式绵云",price:"23.0"},
-          {src:"../../../static/img/coffee2.png",name:"焦糖玛奇朵",price:"33.0"},
-          {src:"../../../static/img/coffee3.png",name:"卡布奇诺",price:"47.0"},
-          {src:"../../../static/img/coffee4.png",name:"哈哈哈哈哈哈哈",price:"38.0"},
-          {src:"../../../static/img/coffee1.png",name:"美式绵云",price:"23.0"},
-          {src:"../../../static/img/coffee2.png",name:"焦糖玛奇朵",price:"33.0"},
-          {src:"../../../static/img/coffee3.png",name:"卡布奇诺",price:"47.0"},
-          {src:"../../../static/img/coffee4.png",name:"哈哈哈哈哈哈哈",price:"38.0"},
-          {src:"../../../static/img/coffee1.png",name:"美式绵云",price:"23.0"},
-          {src:"../../../static/img/coffee2.png",name:"焦糖玛奇朵",price:"33.0"},
-          {src:"../../../static/img/coffee3.png",name:"卡布奇诺",price:"47.0"},
-          {src:"../../../static/img/coffee4.png",name:"哈哈哈哈哈哈哈",price:"38.0"},
-        ]
+
+      }
+    },
+    computed:{
+      list(){
+        return store.state.list
+      },
+      num() {
+        return store.state.num
+      }
+    },
+    methods:{
+      go(){
+        wx.navigateTo({url:"../gowuche/main"})
+      },
+      increase(index){
+        store.commit('increase',index)
+      },
+      decrease(index){
+        if(this.list[index].num>0){
+          store.commit('decrease',index)
+        }
       }
     }
   };
@@ -73,16 +86,51 @@
             background: #159f9f;
             color:#fff;
             opacity: 0.6;
-            border-bottom: 1px solid #cdcdcd;
+            bincrease-bottom: 1px solid #cdcdcd;
           }
         }
   }
   section{
     width:100%;
+    ul{
+      /*position: relative;*/
+      width:100%;
+      height:100%;
+      .addgo,.gouwuche{
+          position: fixed;
+          right:5px;
+          top:900rpx;
+          width:70rpx;
+          height:70rpx;
+          box-shadow:0px 0px 15px -1px #888888;
+          border:1px solid #cdcdcd;
+          -webkit-border-radius:50%;
+          -moz-border-radius: 50%;
+          border-radius: 50%;
+          padding: 10rpx;
+      }
+      .addgo{
+        span{
+          position: absolute;
+          right:0;
+          top:0;
+          display: inline-block;
+          background: #ff2222;
+          width:26rpx;
+          height:24rpx;
+          line-height: 22rpx;
+          color:#fff;
+          font-size:18rpx;
+          text-align: center;
+          -webkit-border-radius: 12rpx;
+          -moz-border-radius: 12rpx;
+          border-radius: 9rpx;
+        }
+      }
     li{
       display: flex;
       border-bottom: 1px solid #cdcdcd;
-      padding-bottom: 10rpx;
+      padding: 10rpx 0;
     img{
       width:55%;
       height:200rpx;
@@ -95,7 +143,7 @@
       p{
         width:100%;
         text-align: right;
-        line-height:70rpx;
+        line-height:64rpx;
       }
       p:nth-child(1){
         color:#333;
@@ -105,24 +153,33 @@
         color:#666;
         font-size:36rpx;
         span{
-          color:#fa6789;
+          color:#ff3100;
           font-size:24rpx;
         }
       }
       p:nth-child(3){
         text-align: right;
+        i{
+          display: inline-block;
+          margin:0 10rpx;
+          font-size:32rpx;
+        }
         span{
           display: inline-block;
           border:1px solid #cdcdcd;
+          -webkit-border-radius:50% ;
+          -moz-border-radius: 50%;
           border-radius:50%;
-          width:60rpx;
-          height: 60rpx;
-          line-height:60rpx;
+          width:46rpx;
+          height: 46rpx;
+          line-height:46rpx;
           text-align:center;
-          color:darkred;
+          color:#ff3100;
+          font-size:40rpx;
         }
       }
     }
     }
+  }
   }
 </style>
--- src/utils/store.js
+++ src/utils/store.js
@@ -11,7 +11,23 @@ const store = new Vuex.Store({
       width: 30,
       height: 30,
       address:""
-    }]
+    }],
+    list:[
+      {src:"../../../static/img/coffee1.png",name:"美式绵云",price:"23.0",num:0},
+      {src:"../../../static/img/coffee2.png",name:"焦糖玛奇朵",price:"33.0",num:0},
+      {src:"../../../static/img/coffee3.png",name:"卡布奇诺",price:"47.0",num:0},
+      {src:"../../../static/img/coffee4.png",name:"哈哈哈哈哈哈哈",price:"38.0",num:0},
+      {src:"../../../static/img/coffee1.png",name:"美式绵云",price:"23.0",num:0},
+      {src:"../../../static/img/coffee2.png",name:"焦糖玛奇朵",price:"33.0",num:0},
+      {src:"../../../static/img/coffee3.png",name:"卡布奇诺",price:"47.0",num:0},
+      {src:"../../../static/img/coffee4.png",name:"哈哈哈哈哈哈哈",price:"38.0",num:0},
+      {src:"../../../static/img/coffee1.png",name:"美式绵云",price:"23.0",num:0},
+      {src:"../../../static/img/coffee2.png",name:"焦糖玛奇朵",price:"33.0",num:0},
+      {src:"../../../static/img/coffee3.png",name:"卡布奇诺",price:"47.0",num:0},
+      {src:"../../../static/img/coffee4.png",name:"哈哈哈哈哈哈哈",price:"38.0",num:0},
+    ],
+    carts:[],
+    num:0
   },
   mutations: {
     addAddress(state) {
@@ -23,8 +39,62 @@ const store = new Vuex.Store({
           console.log(_this.state.markers[0].address)
         }
       })
+    },
+    increase(state,index){
+      this.state.list[index].num++
+      this.state.num++
+      var a = 0
+      var i = 0
+      if(this.state.carts.length>0){
+        for (var i = 0; i < this.state.carts.length; i++){
+          if(this.state.carts[i].name != this.state.list[index].name){
+           a = 1
+          }else{
+           a = 0
+            i = i
+            return a,i
+          }
+        }
+      }else{
+        this.state.carts.push(this.state.list[index])
+      }
+      if(a==0){
+        this.state.carts[i].num+1
+      }else if(a==1){
+        this.state.carts.push(this.state.list[index])
+      }
+    },
+    decrease(state,index){
+      this.state.list[index].num --
+      this.state.num --
+      // var name=this.state.list[index].name
+      // var i=0
+      // console.log(i)
+      // console.log(this.state.carts,name)
+      // if(this.state.carts.length>0) {
+      //   for (i = 0; i < this.state.carts.length; i++) {
+      //     if (this.state.carts[i].name == name) {
+      //       i = i
+      //       return i
+      //     }
+      //   }
+      // }
+      // console.log(i)
+      // if(this.state.list[index].num>0){
+      //   console.log(i)
+      //   this.state.carts[i].num --
+      //   console.log(this.state.carts[i].num)
+      //   if(this.state.carts[i].num==0){
+      //     console.log(i)
+      //     console.log(this.state.carts[i].num)
+      //     this.state.carts.splice(i,1)
+      //   }
+      // }
+
+      }
+
     }
-  }
+
     })
 
 
